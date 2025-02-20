cc5a9f36b636e2b197a66e69b9bba1b5aef08750
Isaac Ciravolo <isaac.ciravolo@gmail.com>
2025-02-19 21:40:11 -0600

    may want to tweak the look but everything works pretty well i think

--- src/User/AssignmentsView.js
+++ src/User/AssignmentsView.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { fetchUserAssignments } from "../firebase/DatabaseFunctions";
 import { useAuth } from "../firebase/AuthContext";
+import { Paper, Button, Typography } from "@mui/material";
 const AssignmentsView = () => {
   const [assignments, setAssignments] = useState([]);
   const { currentUser: user } = useAuth();
@@ -12,7 +13,36 @@ const AssignmentsView = () => {
     });
   }, [user]);
 
-  return <>{assignments && JSON.stringify(assignments)}</>;
+  return (
+    <>
+      {/* {assignments && JSON.stringify(assignments)} */}
+      <div>
+        {assignments.map((assignment) => (
+          <Paper
+            key={assignment.id}
+            sx={{
+              p: 2,
+              m: 2,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: 400,
+            }}
+          >
+            <Typography>{assignment.title}</Typography>
+            <Button
+              variant="contained"
+              href={assignment.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GO
+            </Button>
+          </Paper>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default AssignmentsView;
