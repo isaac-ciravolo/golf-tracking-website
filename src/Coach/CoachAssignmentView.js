b5d555c45d028a291fe2d9a81a0d0c2a047ee3ff
Isaac Ciravolo <isaac.ciravolo@gmail.com>
2025-02-21 10:08:15 -0600

    updated todo and some coach johns

--- src/Coach/CoachAssignmentView.js
+++ src/Coach/CoachAssignmentView.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "../firebase/AuthContext";
 import { fetchClassAssignments } from "../firebase/DatabaseFunctions";
 import { useParams } from "react-router-dom";
+import { Paper, Button, Typography } from "@mui/material";
 const CoachAssignmentView = () => {
   const { id: classCode, assignmentId } = useParams();
   const [assignment, setAssignment] = useState(null);
@@ -15,6 +16,34 @@ const CoachAssignmentView = () => {
     temp();
   }, []);
 
-  return <>{assignment && JSON.stringify(assignment)}</>;
+  return (
+    <>
+      {/* {assignment && JSON.stringify(assignment)} */}
+      <Paper
+        sx={{
+          p: 2,
+          m: 2,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: 400,
+        }}
+      >
+        <Typography>{assignment?.title}</Typography>
+
+        <Button
+          variant="contained"
+          href={assignment?.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GO
+        </Button>
+      </Paper>
+      {assignment?.students.map((student) => (
+        <Typography key={student}>{student}</Typography>
+      ))}
+    </>
+  );
 };
 export default CoachAssignmentView;
